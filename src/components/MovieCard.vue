fdd22ca9c4f01624cf92bbe7ec1e7b8a2764913b
Matheus Gonçalves <55060810+matheusgbl@users.noreply.github.com>
2021-08-06 14:31:08 -0300

    Adicionado estilo aos cards dos filmes

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -7,9 +7,16 @@
 <style scoped>
 .movie_card {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  max-width: 13rem;
-  height: 20rem;
+  max-width: 15rem;
+  height: 30rem;
   text-align: center;
   margin-top: 30px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  transition: all 1s;
+}
+
+.movie_card:hover {
+  transform: scale(1.1);
 }
 </style>
--- src/components/MovieCardDetail.vue
+++ src/components/MovieCardDetail.vue
@@ -4,16 +4,16 @@
     <img class="movie_image" :src="`https://image.tmdb.org/t/p/w200${movie.img}`">
       <span class="movie_date">{{movie.date}}</span>
       <div class="movie_card_info">
-        <p class="movie_card_name">{{movie.title}}</p>
-        <div class="card_rate_gender">
-        <p>{{movie.rate}}</p>
+        <p class="movie_card_title"><strong>{{movie.title}}</strong></p>
+        <div class="card_rate_genre">
+        <p><strong>{{movie.rate}}</strong></p>
         <p>{{movie.genre}}</p>
         </div>
       </div>
       <div class="price">
         <p>R$ 79,99</p>
       </div>
-      <button>Adicionar</button>
+    <button class="buy_button">Adicionar</button>
     </base-card>
   </section>
 </template>
@@ -36,17 +36,58 @@ export default {
 .movie_card_list {
   align-items: center;
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-  gap: 20px;
+  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  gap: 30px;
+  font-family: Arial, Helvetica, sans-serif;
 }
 
 .movie_image {
   margin: 0 auto;
   display: flex;
   justify-content: center;
-  padding: 10px;
-  width: 210px;
-  height: 230px;
+  width: 240px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
+
+.movie_card_title {
+  overflow: hidden;
+  width: 100%;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical
+}
+
+.price {
+  margin-top: 5px;
+}
+
+.card_rate_genre {
+  display: flex;
+  justify-content: center;
+}
+
+.card_rate_genre > p {
+  margin-left: 10px;
+}
+
+.buy_button {
+  background-color: #6558f5;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin-top: 1.5rem;
+  width: 100%;
+  align-items: center;
+  color: #fff;
+  font-weight: 600;
+  border: none;
+  padding: 12px;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+}
+
+.buy_button:hover {
+  cursor: pointer;
+  background-color: #3d33a8;
 }
 
 </style>
